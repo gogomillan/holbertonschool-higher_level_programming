799023b6f0d0c5c96bb434e3cfbbfebdd9c143d7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-05 15:27:00 +0000

    Linked list cycle commit! (#10)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
+/**
+ * check_cycle - Detect a loped linked list
+ * @list: list head
+ *
+ * Return: 0 false, 1 true
+ */
 int check_cycle(listint_t *list)
 {
 listint_t *current, *verify;
 
 	current = list;
-	while (current != NULL)
+	verify = list;
+	while (current != NULL && verify != NULL && verify->next != NULL)
 	{
-		if (current->next != NULL)
-		{
-			verify = current->next;
-			do {
-				if (verify == current)
-					return (1);
-				verify = verify->next;
-			} while (verify != NULL);
-		}
-
 		current = current->next;
+		verify = verify->next->next;
+		if (current == verify)
+			return (1);
 	}
 
 	return (0);
