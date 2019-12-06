1e1b061ccaf73ff252c6c68589b0755d92b75d58
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-06 01:54:22 +0000

    Insert in sorted linked list commit! (#13)

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - adds a new node in ordered way in listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *current, *prev, *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	current = *head;
+	prev = current;
+	new->n = number;
+	if (current->n > number)
+	{
+		new->next = *head, *head = new;
+		return (new);
+	}
+
+	while (current != NULL)
+	{
+		if (current->n > number)
+		{
+			prev->next = new, new->next = current;
+			return (new);
+		}
+		prev = current;
+		current = current->next;
+	}
+	prev->next = new, new->next = NULL;
+
+	return (new);
+}
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    mod = number % 10
-    print(mod, end='')
-    return (mod)
