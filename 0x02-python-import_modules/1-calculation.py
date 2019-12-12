6b324a3a6f84a76d1d628b736c66e134437f039f
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-12 19:13:35 +0000

    Using alias in import (#1)

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    import calculator_1 as c
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, c.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, c.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, c.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, c.div(a, b)))
