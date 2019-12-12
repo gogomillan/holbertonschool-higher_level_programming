6b9ea09272d9d5c903c8f2167c796b67c061c2b0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-12 19:57:07 +0000

    Fix the counter of parameters (#2)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 2:
-        print("{:d} argument.".format(len(sys.argv)-1))
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv)-1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
     else:
         print("{:d} arguments:".format(len(sys.argv)-1))
     i = 0
