1fc4997a9c630ad642f877da9599a8534973bbff
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 20:13:32 +0000

    Fix the creation of the new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = []
-    new.append(list(map((lambda x: x if x != search else replace), my_list)))
+    new = (list(map((lambda x: x if x != search else replace), my_list)))
     return new
