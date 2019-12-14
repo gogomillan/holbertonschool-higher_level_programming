54941f9bebc475464958ef7ca19a4bd885e49f4e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 11:06:42 +0000

    Fix, replace or by and

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,5 @@
 def no_c(my_string):
     if __name__ != '__main__':
         new = ''
-        if my_string is None:
-            return (new)
-        new = new.join([i for i in my_string if i != 'C' or i != 'c'])
+        new = new.join([i for i in my_string if i != 'C' and i != 'c'])
         return (new)
