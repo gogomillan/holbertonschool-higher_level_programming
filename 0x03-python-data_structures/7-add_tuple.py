d1d20414bfdd827f5afb2427900f3bac8dda2848
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 12:46:22 +0000

    Tuples addition, commit! (#7)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     c = [0, 0]
-    for i in range(len(tuple_a)):
+    limit = len(tuple_a) if len(tuple_a) <= 2 else 2
+    for i in range(limit):
         c[i] = c[i] + tuple_a[i]
-    for i in range(len(tuple_b)):
+    limit = len(tuple_b) if len(tuple_b) <= 2 else 2
+    for i in range(limit):
         c[i] = c[i] + tuple_b[i]
     return (tuple(c))
