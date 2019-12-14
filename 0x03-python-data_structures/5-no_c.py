2657f6c216b28f082edfd9cda2e51932c57dfaf0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 10:59:24 +0000

    Can you C me now? Commit! (#5)

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if __name__ != '__main__':
+        if my_string is None:
+            return
+        new = ''
+        new = new.join([i for i in my_string if i != 'C' or i != 'c'])
+        return (new)
