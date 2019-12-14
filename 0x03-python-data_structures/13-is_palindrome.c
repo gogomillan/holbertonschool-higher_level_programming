4a5bab46a8c2411a4f90a83bf4c3b5890b4098b3
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 15:46:50 +0000

    Linked list palindrome, commit! (#13)

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: Head of the list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+int *list;
+long go = 1, i;
+listint_t *forw;
+
+	if (head == NULL)
+		return (1);
+	if (*head == NULL)
+		return (1);
+
+	forw = *head;
+	while (forw->next != NULL)
+		forw = forw->next, go++;
+	list = malloc(go * sizeof(int));
+    if (!list)
+		return (0);
+	forw = *head, go = 0;
+	while (forw != NULL)
+        list[go] = forw->n, forw = forw->next, go++;
+
+	for (i = 0; i < (go / 2); i++)
+	{
+		if (list[i] != list[go - i - 1])
+			return (0);
+	}
+
+	return (1);
+}
