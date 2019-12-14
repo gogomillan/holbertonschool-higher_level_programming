4e007ecd0ecf97e7d3a46448a4983bfdeac7d5d7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 23:11:15 +0000

    Control string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     summ = 0
     val = 0
