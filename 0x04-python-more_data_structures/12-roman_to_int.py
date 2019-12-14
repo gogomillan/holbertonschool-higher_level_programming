d1b7a3ea937de29350d5798e82c4a5514c37b970
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-14 22:55:29 +0000

    Roman to Integer, commit! (#12)

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    values = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    summ = 0
+    val = 0
+    for k in roman_string:
+        if values[k] > val:
+            summ = values[k] - summ
+        else:
+            summ = summ + values[k]
+        val = values[k]
+    return summ
