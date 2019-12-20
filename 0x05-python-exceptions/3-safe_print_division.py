97f45f123cecb87f3ddcff564b337a209c96a7db
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-20 11:31:39 +0000

    Integers division with debug commit! (#3)

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,8 +5,5 @@ def safe_print_division(a, b):
     except:
         c = None
     finally:
-        if c is None:
-            print("Inside result: {}".format(c))
-        else:
-            print("Inside result: {:.1f}".format(c))
+        print("Inside result: {}".format(c))
         return c
