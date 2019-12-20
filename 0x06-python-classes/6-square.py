e1fe49680255e3a2bec4f5e66c980055e8322f27
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-20 22:54:01 +0000

    Fix cntrl print of white spaces

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,7 +72,7 @@ class Square:
         """ This method prints in stdout the square with the character #.
         """
         for i in range(self.__size):
-            if self.__position[1] == 0:
+            if self.__position[1] <= 0:
                 print(" " * self.__position[0], end='')
             print("#" * self.__size)
         if self.__size == 0:
