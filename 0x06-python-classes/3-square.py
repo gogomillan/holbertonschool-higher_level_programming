ee1732f660a79379e15a60b1f56f2d7656ce449e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-20 21:38:32 +0000

    Area of a square (#3)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """ This is a class to define a square.
+    """
+    def __init__(self, size=0):
+        """ This is constructr method.
+
+        Args:
+            size: size of the square.
+        """
+        if not type(size) is int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """ This method return the area of the square.
+
+        Returns:
+            area: The are of the square.
+        """
+        return self.__size * self.__size
