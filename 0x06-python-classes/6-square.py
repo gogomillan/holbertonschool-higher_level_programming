96da975db35fb6595238a67923c8c76f1e04c515
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-21 00:03:33 +0000

    Fix the property and setter

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,12 +68,10 @@ class Square:
     def my_print(self):
         """ This method prints in stdout the square with the character #.
         """
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
         if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+            print()
