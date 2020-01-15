ff0cb0dbee01e8aef543332aeaa5217192cd9854
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-15 21:24:44 +0000

    Fix pep8 style whitespaces

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,6 +7,6 @@ Module for LockedClass with no class or object attribute.
 class LockedClass:
     def __setattr__(self, key, value):
         if key is not "first_name":
-            raise AttributeError("'LockedClass' object has no attribute"+
-                                " 'last_name'")
+            raise AttributeError("'LockedClass' object has no attribute\
+ 'last_name'")
         object.__setattr__(self, key, value)
