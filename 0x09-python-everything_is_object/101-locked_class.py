f10caa0ea59ca78330e2f44600468f3c919749a9
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-15 21:27:35 +0000

    Fix the control attribute

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-"""
-Module for LockedClass with no class or object attribute.
-"""
-
-
 class LockedClass:
-    def __setattr__(self, key, value):
-        if key is not "first_name":
-            raise AttributeError("'LockedClass' object has no attribute\
- 'last_name'")
-        object.__setattr__(self, key, value)
+    __slots__ = ['first_name']
