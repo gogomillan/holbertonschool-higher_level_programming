00f02d9d74bbf68fbe1ee702f2d41051521a09fb
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-15 19:43:45 +0000

    Low memory cost commit! (#31)

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for LockedClass with no class or object attribute.
+"""
+
+
+class LockedClass:
+    def __setattr__(self, key, value):
+        if key is not "first_name":
+            raise AttributeError("'LockedClass' object has no attribute"+
+                                " 'last_name'")
+        object.__setattr__(self, key, value)
