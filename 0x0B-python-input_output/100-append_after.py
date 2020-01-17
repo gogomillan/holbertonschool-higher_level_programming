228ed9d2afdb3a2ad13e638baae0a08790acfd5d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-17 13:54:08 +0000

    Search and update commit!

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -14,12 +14,12 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The searching string
         new_string (str): The new line string
     """
-    with open(filename, 'r', encoding='utf-8') as myFile:
-        lines = myFile.readlines()
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
 
     for i, line in enumerate(lines):
         if search_string in line:
             lines.insert(i + 1, new_string)
-    with open(filename, 'w', encoding='utf-8') as myFile:
+    with open(filename, 'w', encoding='utf-8') as f:
         content = "".join(lines)
-        myFile.write(content)
+        f.write(content)
