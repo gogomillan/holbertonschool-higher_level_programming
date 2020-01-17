7c5a13a66c50f7dc1b5578fdf920cc8b84d8fd2d
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-17 04:07:52 +0000

    Can I? commit! (#13)

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,10 +9,11 @@ def add_attribute(obj, name, value):
     Function to set atributes in a object.
 
     Args:
+        obj (object): The object
         name (str): The name of the object
         value (object): The value of the object
     """
-    if str(type(obj)).find("__main__") != -1:
+    if str(type(obj)).find(".") != -1:
         setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
