2da4f316018de32bb6f9ad15e334541cd5f7d590
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-17 03:34:05 +0000

    Fix pep8

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,7 @@ object is an instance of, or if the object is an instance of a class that
 inherited from, the specified class; otherwise False.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
     Function that determine if a class is a specific class or an inherited
