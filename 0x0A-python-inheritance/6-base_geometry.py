0d52ecbed8b039fd8240bb997d526e7a1ee2bf38
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-17 02:41:06 +0000

    Improve Geometry commit! (#6)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Method: def area(self): that raises an Exception with the message
+        area() is not implemented
+        """
+        raise Exception("area() is not implemented")
