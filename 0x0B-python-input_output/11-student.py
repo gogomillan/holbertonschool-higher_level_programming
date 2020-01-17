23eeda268678d691c58c009a5c739e9976e89b45
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-17 11:52:50 +0000

    Fix pep8 style

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
-        self.age =  age
+        self.age = age
 
     def to_json(self):
         """
