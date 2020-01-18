3e33408824ae1ab8058f6a371b7fdd3e24cf678b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-01-18 04:11:02 +0000

    Update #0 commit! (#8)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -152,3 +152,21 @@ class Rectangle(Base):
                                                                 self.width,
                                                                 self.height)
         return ret
+
+    def update(self, *args):
+        """
+        Update the value of the atributes
+
+        Args:
+            args (variable arguments)
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
