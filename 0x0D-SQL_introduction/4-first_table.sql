cac15214df1415fafffc18c8514204e38e4a963b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-25 14:53:33 +0000

    Creating first_table (#4)

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table called first_table in the current database in a MySQL server.
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table
+(
+  id INT, 
+  name VARCHAR(256)
+);
