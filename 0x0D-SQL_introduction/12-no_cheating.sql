112604dda6c6ec688973a7ba3426f9ce25595c52
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-25 16:27:23 +0000

    Cheating is bad (#12)

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Script that updates the score of Bob to 10 in the table second_table of the database hbtn_0c_0 in a MySQL server.
+-- Update with where
+UPDATE second_table
+SET    score = 10
+WHERE  name  = "Bob";
