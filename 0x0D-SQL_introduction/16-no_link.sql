714b54c2e419dcde53e07a38ff3edb0fba2f345e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-25 17:07:55 +0000

    Say my name (#16)

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Script that lists all records of the table second_table of the database hbtn_0c_0 in a MySQL server.
+-- Where no list rows without a name value
+SELECT score, name
+FROM   second_table
+WHERE  name IS NOT NULL
+ORDER BY score DESC;
