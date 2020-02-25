be8ba1d4df68f6ec8726ce279c973503e7556a83
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-25 14:28:14 +0000

    Delete a database (#2)

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Script that creates the database hbtn_0c_0 in a MySQL server.
--- Create a database
+-- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Script that deletes the database hbtn_0c_0 in a MySQL server.
+-- Drop database
+DROP DATABASE IF EXISTS hbtn_0c_0;
