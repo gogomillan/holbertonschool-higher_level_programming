1692b2fee07773405ffdd232e10d28ca25991719
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-26 05:24:01 +0000

    My privileges (#0)

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on a server (in localhost).
+-- Show grants for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Show grants for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
