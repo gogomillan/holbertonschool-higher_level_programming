71deeaaa1998c231b287cb25ed0074c15e996b85
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-02-26 06:26:02 +0000

    States table commit! (#6)

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,7 @@
 -- Script that creates the table unique_id on a MySQL server.
 -- Create table
-CREATE TABLE IF NOT EXISTS unique_id (
+CREATE TABLE IF NOT EXISTS unique_id
+(
   id INT DEFAULT 1 UNIQUE,
   name VARCHAR(256)
 ) ENGINE=InnoDB;
