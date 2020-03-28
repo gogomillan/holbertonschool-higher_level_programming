1f4c0d9b09aef0827c13264126368107c621bd7b
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-03-28 03:03:31 +0000

    Filter states by user input, commit! (#2)

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Script  that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+
+
+def main(db_user="root", db_passwd="root", db_name="hbtn_0e_0_usa",
+         fltr_name="'%'"):
+    """
+    Function lists states from the database hbtn_0e_0_usa filtered
+
+    Args:
+        db_user (str): Database user name
+        db_passwd (str): Database user password
+        db_name (str): Database user password
+        fltr_name (str): State name to find (filter)
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=db_user,
+                           passwd=db_passwd, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    stmt = "SELECT * \
+            FROM   states \
+            WHERE  name LIKE '{}' \
+            ORDER BY id ASC".format(fltr_name)
+    cur.execute(stmt)
+
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 5:
+        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
+        exit(1)
+
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
