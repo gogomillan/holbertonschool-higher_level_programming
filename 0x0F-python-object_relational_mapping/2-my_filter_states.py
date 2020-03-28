c67fdc575e58e6fb407ddc40bc925b243ec7d7d2
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-03-28 19:48:42 +0000

    Assurance case sensitive control 2-my_filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ def main(db_user="root", db_passwd="root", db_name="hbtn_0e_0_usa",
     cur = conn.cursor()
     stmt = "SELECT * \
             FROM   states \
-            WHERE BINARY name = '{}' \
+            WHERE UPPER(name) = UPPER('{}') \
             ORDER BY id ASC".format(fltr_name)
     cur.execute(stmt)
 
