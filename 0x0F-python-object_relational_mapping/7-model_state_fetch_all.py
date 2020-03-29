0c6d958ed786ee780a2044635faede1f4e2a9eaa
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-03-29 10:14:48 +0000

    Fix warning msg from orm

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,6 +3,7 @@
 Module that lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
+import warnings
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import Session
@@ -12,6 +13,7 @@ def main():
     """
     Main function for the Module
     """
+    warnings.simplefilter('ignore', Warning)
     cs = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
                                                      sys.argv[2], sys.argv[3])
     engine = create_engine(cs, pool_pre_ping=True)
@@ -23,6 +25,7 @@ def main():
         print("{}: {}".format(state.id, state.name))
 
     session.close()
+    warnings.simplefilter('always', Warning)
 
 if __name__ == "__main__":
     main()
