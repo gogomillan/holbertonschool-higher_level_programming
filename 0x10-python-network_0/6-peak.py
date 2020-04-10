dae916be56372287e07b800494df9da7f5d6bff7
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-09 22:01:18 -0500

    Evaluating binary search for peak but with edge cases

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,42 +14,22 @@ def find_peak(list_of_integers):
     Returns:
         The peak value
     """
-    lof = list_of_integers
-    """ Special cases : no data, single element """
-    if len(lof) == 0:
+    if len(list_of_integers) == 0:
         return None
-    if len(lof) == 1:
-        return lof[0]
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if list_of_integers[1] <= list_of_integers[0]:
+        return list_of_integers[0]
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
 
-    """ Brute force
-        Decide peak left edge
-        Decide peak right edge
-        Decide the peak against its neighbors
-    """
-    peak = 0
-    for i in range(len(lof)):
-        if i == 0 and lof[i] > lof[i + 1]:
-            peak = lof[i] if lof[i] > peak else peak
-        elif i == len(lof) - 1 and lof[i] > lof[i - 1]:
-            peak = lof[i] if lof[i] > peak else peak
-        elif (i == 0 or lof[i] >= lof[i - 1]) \
-                and \
-             (i == len(lof) - 1 or lof[i] >= lof[i + 1]):
-            peak = lof[i] if lof[i] > peak else peak
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] \
+            and list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
 
-    return peak
+    if list_of_integers[mid + 1] > list_of_integers[mid]:
+        return(find_peak(list_of_integers[mid + 1:len(list_of_integers)]))
 
-    """ Binary Searcha
-        - Decides the middle element
-        - elif lower half call
-        - else higher half call
-    mid = len(lof) // 2
-    print("{}, ".format(lof[mid]), end="")
-    if (mid == 0 or lof[mid - 1] <= lof[mid]) \
-            and (mid == len(lof) - 1 or lof[mid + 1] <= lof[mid]):
-        return lof[mid]
-    elif lof[mid - 1] > lof[mid]:
-        return(find_peak(lof[0:mid - 1]))
-    else:
-        return(find_peak(lof[mid:len(lof)]))
-    """
+    if list_of_integers[mid - 1] > list_of_integers[mid]:
+        return(find_peak(list_of_integers[0:mid]))
