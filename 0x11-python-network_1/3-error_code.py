8c1c090865df55be15ce582011c741054f01d485
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-15 14:04:48 -0500

    Error code, commit!

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8), and manages urllib.error.HTTPError
+exceptions.
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    if url is not None:
+        req = urllib.request.Request(url)
+        try:
+            response = urllib.request.urlopen(req)
+            the_page = response.read()
+            print(the_page.decode('utf-8'))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
