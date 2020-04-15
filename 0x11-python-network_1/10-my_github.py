efa378a59fe1097f596c357da97f00f9500f102a
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-04-15 18:20:51 -0500

    Fix PEP8 style and add json method

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -12,13 +12,4 @@ if __name__ == "__main__":
     token = sys.argv[2]
 
     r = requests.get('https://api.github.com/user', auth=(user, token))
-
-    if r.status_code >= 400:
-        print("None")
-    else:
-        """ It is used ||| as token because of dates has : inside
-        """
-        r_dict = dict([e.split("|||") for e in r.text.strip("{}\n").
-                       replace('":"', '"|||"').replace('":', '"|||').
-                       replace('"', '').split(",")])
-        print("{id}".format(**r_dict))
+    print(r.json().get("id"))
