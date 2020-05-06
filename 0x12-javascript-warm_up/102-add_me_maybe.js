9ef4bf79c307de53951531145836911748f6292c
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-05 23:22:08 -0500

    Add me maybe

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+   function that increments and calls a function.
+   - The function is visible from outside
+   - Prototype: function (number, theFunction)
+ */
+exports.addMeMaybe = function (x, theFunction) {
+    theFunction(number + 1);
+  }
+};
