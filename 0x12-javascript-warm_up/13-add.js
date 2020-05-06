8e778d42b0cfc5a9a9e90301a0b6dce50b156271
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-05 22:56:43 -0500

    Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+   function that returns the addition of 2 integers.
+   - The function must be visible from outside
+   - The name of the function is add
+ */
+function add (a, b) {
+  return a + b;
+}
+module.exports.add = add;
