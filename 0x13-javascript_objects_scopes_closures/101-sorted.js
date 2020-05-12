102abb60d7fbaef401ebde9c928105003d45e047
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-11 23:41:53 -0500

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+   script that imports an array and order into a new array.
+ */
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
