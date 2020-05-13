347334bbde85e491cc6a031afb9a9054e2d2dca9
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-12 23:33:40 -0500

    Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+  script that prints all characters of a Star Wars movie:
+  - The first argument is the movie ID
+  - It is used the Star wars API with the endpoint
+    https://swapi-api.hbtn.io/api/films/:id
+  - It is used the module request
+ */
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  for (const each of JSON.parse(body).characters) {
+    request(each, function (err, resp, bd) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(JSON.parse(bd).name);
+    });
+  }
+});
