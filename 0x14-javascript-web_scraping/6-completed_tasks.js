4f88e09705a90f56b831e4b30ce4eb868c718661
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-12 21:37:57 -0500

    How manu completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+  script that computes the number of tasks completed by user id.
+  - The first argument is the API URL:
+    https://jsonplaceholder.typicode.com/todos
+  - It is used the module request
+ */
+const request = require('request');
+const url = process.argv[2];
+const users = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    for (const user of JSON.parse(body)) {
+      if (!users[parseInt(user.userId, 10) - 1]) {
+        users[parseInt(user.userId, 10) - 1] = 0;
+      }
+      users[parseInt(user.userId, 10) - 1] += 1;
+    }
+    console.log(JSON.stringify(users));
+  }
+});
