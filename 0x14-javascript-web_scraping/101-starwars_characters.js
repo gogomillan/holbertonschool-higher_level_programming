454c9e1e580fac8c0c386f98cef3e8284e37e1b0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-12 23:48:27 -0500

    Add promisses

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,16 +9,28 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  }
-  for (const each of JSON.parse(body).characters) {
-    await request(each, function (err, resp, bd) {
-      if (err) {
-        console.log(err);
+function listOrderedCharacters (url) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const charDict = {};
+      const charList = JSON.parse(body).characters;
+      for (let i = 0; i < charList.length; i++) {
+        request(charList[i], function (error, response, body) {
+          if (error) {
+            console.log(error);
+          } else {
+            charDict[i] = JSON.parse(body).name;
+          }
+          if (charList.length === Object.keys(charDict).length) {
+            for (let k = 0; k < Object.keys(charDict).length; k++) {
+              console.log(charDict[k]);
+            }
+          }
+        });
       }
-      console.log(JSON.parse(bd).name);
-    });
-  }
-});
+    }
+  });
+}
+listOrderedCharacters(url);
