a35de120f78fa47004a7d9d5926691cb20e8997b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2020-05-14 18:31:01 -0500

    No jQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+/*
+  script that updates the text color of the HTML tag HEADER to red (#FF0000):
+  - It is used document.querySelector to select the HTML tag
+ */
+document.getElementById('header').style.color='#FF0000';
